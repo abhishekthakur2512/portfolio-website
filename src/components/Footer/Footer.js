1cecebe244e30234ac130c76385df0618b8ee63a
Abhishek Thakur <abhishekthakur2596@gmail.com>
2021-04-17 05:09:26 +0530

    icons footer fix navigate and copy

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import linkedin_logo from '../../images/linkedin_logo.png'
 import phone_logo from '../../images/phone_logo.png';
 import instagram_logo from '../../images/instagram_logo.png';
@@ -10,20 +10,35 @@ import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 
 function Footer() {
+  const [isPhoneCopied, setIsPhoneCopied] = useState(false);
+  const handleLinkedInClick = () => {
+    const url = 'https://www.linkedin.com/in/abhishekthakur3/';
+    const win = window.open(url);
+    win.focus();
+  }
+
+  const handlePhoneClick = () => {
+    console.log('phone number');
+    navigator.clipboard.writeText('7838150052');
+    setIsPhoneCopied(true);
+  }
   return (
     <div style = {contact_logo}>
       <hr style={{width:"50%", align: "center", marginTop: '50px'}}/> 
-      <CopyToClipboard text={'hello'}>
-          <button data-tip data-for="linkedin" style= {{background: 'transparent', border: 'none', outline: 'none'}}>
-              <img src={ linkedin_logo } className='contact-logo'></img>
-          </button>
-      </CopyToClipboard>
-      <button data-tip data-for="phone" style= {{background: 'transparent', border: 'none', outline: 'none'}}><img src={ phone_logo } className='contact-logo'></img></button>
+      <button onClick = {handleLinkedInClick} style= {{background: 'transparent', border: 'none', outline: 'none'}}>
+        <img src={ linkedin_logo } className='contact-logo'></img>
+      </button>
+
+      <button onClick = {handlePhoneClick} data-tip data-for="phone" style= {{background: 'transparent', border: 'none', outline: 'none'}}>
+        <img src={ phone_logo } className='contact-logo'></img>
+      </button>
       <button data-tip data-for="instagram" style= {{background: 'transparent', border: 'none', outline: 'none'}}><img src={ instagram_logo } className='contact-logo'></img></button>
       <button data-tip data-for="email" style= {{background: 'transparent', border: 'none', outline: 'none'}}><img src={ email_logo } className='contact-logo'></img></button>
 
       <ReactTooltip id="linkedin" border='none' place="bottom" effect="solid">linkedin.com/in/abhishekthakur3</ReactTooltip>
-      <ReactTooltip id="phone" place="bottom" effect="solid">+91 7838150052</ReactTooltip>
+      <ReactTooltip id="phone" place="bottom" effect="solid">
+        { isPhoneCopied ? 'Copied!': '+91 7838150052' }
+      </ReactTooltip>
       <ReactTooltip id="instagram" place="bottom" effect="solid">abhishekk2512</ReactTooltip>
       <ReactTooltip id="email" place="bottom" effect="solid">abhishekthakur251296@gmail.com</ReactTooltip>
     </div>
