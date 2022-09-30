5fc7cac25e7ad686b2d2e31706283052d7f4ce53
abhishekthakur2512 <abhishekthakur2596@gmail.com>
2022-09-30 20:49:37 +0530

    test training

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import { useNavigate } from 'react-router-dom';
 // import logo from './logo.svg';
 import './App.css';
 import UCTest from './components/UCTest/UCTest';
-import Wrapper from './components/Wrapper/Wrapper'
 
 
 class App extends Component{
