71c1329b47aab9770b583fc6c6b101f389bda5cf
abhishekthakur2512 <abhishekthakur2596@gmail.com>
2022-09-30 20:47:41 +0530

    new site change

---
+++ src/components/UCTest/UCTest.js
@@ -0,0 +1,18 @@
+import { Button } from '@material-ui/core';
+import React  from 'react';
+function UCTest() {
+  
+  const openNewPage = () => {
+    window.open("https://partnerapp.urbanclap.com/calendarTrainingCompleted");
+  };
+
+    return (
+        <div>
+          <Button onClick={openNewPage}>CHECK</Button>
+        </div>
+    )
+}
+
+export default UCTest
+
+// onCopy={() => this.setState({copied: true})}
