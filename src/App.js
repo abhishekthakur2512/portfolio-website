fc1458d570651249c642f580ebd0766895183910
abhishekthakur2512 <abhishekthakur2596@gmail.com>
2025-03-06 01:17:19 +0530

    dad game push

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import logo from './logo.svg';
 import './App.css';
 import Wrapper from './components/Wrapper/Wrapper'
+import Dad from './components/Dad/dad';
+
 
 
 class App extends Component{
-
-  render(){
-      return (
-          <Wrapper/>
-      );
+  render() {
+    return (
+      <Routes>
+        <Route path="/" element={<Wrapper />} />
+        <Route path="/dad" element={<Dad />} />
+      </Routes>
+    );
   };
 }
    
